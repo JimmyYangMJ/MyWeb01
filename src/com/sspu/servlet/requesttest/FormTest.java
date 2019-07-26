7046e061edcb3c2e5df1c72e326de19e82fcaf4c
YMj1998 <2237895205@qq.com>
2019-07-26 15:50:03 +0800

    Java web

--- src/com/sspu/servlet/requesttest/FormTest.java
+++ src/com/sspu/servlet/requesttest/FormTest.java
@@ -1,4 +1,30 @@
 package com.sspu.servlet.requesttest;
 
-public class FormTest {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+
+@WebServlet("/Servlet/requesttest/FormTest")
+public class FormTest extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String gender = request.getParameter("gender");
+        String remark = request.getParameter("remark");
+        String [] hobbies = request.getParameterValues("hobby");
+
+        System.out.println(username);
+        System.out.println(password);
+        System.out.println(gender);
+        System.out.println(remark);
+        System.out.println(Arrays.toString(hobbies));
+    }
 }
