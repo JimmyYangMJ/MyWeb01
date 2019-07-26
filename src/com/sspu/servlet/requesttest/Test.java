37d8f37a1d79b99d6735b8574f274ac9fcc4826b
YMj1998 <2237895205@qq.com>
2019-07-26 15:42:00 +0800

    请求方法

--- src/com/sspu/servlet/requesttest/Test.java
+++ src/com/sspu/servlet/requesttest/Test.java
@@ -1,12 +1,15 @@
 package com.sspu.servlet.requesttest;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Enumeration;
+import java.util.Map;
 
 @WebServlet("/Servlet/requesttest/Test01")
 public class Test extends HttpServlet {
@@ -76,6 +79,79 @@ public class Test extends HttpServlet {
         response.getWriter().write("xxx");
     }
 
+    /**
+     * 获取URL 参数
+     */
+    private static void parameter(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        /** 获得客户端请求参数(客户端提交的数据) */
+        /**
+         * 1.getParameter(name)方法
+         */
+
+        /** http://localhost:8080/MyWeb01_war_exploded/
+         * Servlet/requesttest/Test01?username=gyf&passowrd=123 */
+		String username = request.getParameter("username");
+		String password = request.getParameter("passowrd");
+		String gender = request.getParameter("gender");
+		System.out.println("客户端浏览器发送来的请求参数?username=gyf&passowrd=123：");
+		System.out.println(username);
+		System.out.println(password);
+		System.out.println(gender);
+
+        /**
+         * 2.getParameterValues（String name）方法 【获取一个参数的多个值】
+         */
+        /** ?hobby=studying & hobby=money */
+		String[] hobbies = request.getParameterValues("hobby");
+		System.out.println("获取一个参数的多个值");
+		System.out.println(Arrays.toString(hobbies));
+
+        /**
+         * 3.getParameterNames方法 【获取所有参数】
+         */
+        /** ?username=gyf&passowrd=123 */
+        /** Enumeration当成集合来用*/
+        System.out.println("获取 所有 参数");
+		Enumeration<String> names = request.getParameterNames();
+		//遍历
+		while(names.hasMoreElements()){
+			System.out.println(names.nextElement());
+		}
+
+        /**
+         *4.getParameterMap方法   【把所有请求参数存在一个map中】//做框架用，非常实用
+         */
+        /** ?username=gyf&passowrd=123&hobby=studing&hobby=money */
+        /**
+         * key			value
+         * username		{"gyf"};
+         * password     {"123"};
+         * hobby		{"studying","money"}
+         */
+		Map<String, String[]> map = request.getParameterMap();
+		for(Map.Entry<String, String[]> entry : map.entrySet()){
+			System.out.println("参数名字:" + entry.getKey());
+
+			System.out.println("参数的值:");
+			for(String value : entry.getValue()){
+				System.out.println(value);
+			}
+		}
+
+
+        /**
+         * 5.getInputStream 【以字节读请求参数】
+         */
+        System.out.println(".....");
+        ServletInputStream is = request.getInputStream();
+        byte[] buf = new byte[1024];
+        int len ;
+        while((len = is.read(buf)) != -1){
+            //把buf转成字符串
+            String s = new String(buf, 0, len);
+            System.out.println(s);
+        }
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -85,5 +161,8 @@ public class Test extends HttpServlet {
         System.out.println("==================");
         /** 获取请求头*/
         requestHeader(request, response);
+        System.out.println("==================");
+        /** 获取URL 参数 */
+        parameter(request, response);
     }
 }
